e4cc18f55780e9be88fbb6727e2d71c46a1b0358
Davide Parpinello <44616921+davideparpinello@users.noreply.github.com>
2021-01-19 22:37:15 +0100

    fixed populate db

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var readChunk = require('read-chunk')
 const FileType = require('file-type')
 const imagesFolder = path.join(__dirname, "public/img");
 
-function populateDB() {
+function populateDB(callback) {
     var imagesDB = [];
     var i = 1;
     fs.readdir(imagesFolder, function (err, files) {
@@ -24,90 +24,91 @@ function populateDB() {
         });
 
         fs.writeFileSync('images.json', JSON.stringify(imagesDB), 'utf-8');
-
+        callback();
     });
 }
 
-populateDB();
+function main() {
+    app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs')
-let imagesFile = fs.readFileSync('images.json')
-let imagesDB = JSON.parse(imagesFile);
+    let imagesFile = fs.readFileSync('images.json')
+    let imagesParsed = JSON.parse(imagesFile);
 
-app.use(express.static('public'))
-app.use('/uploads', express.static('public/img'));
+    app.use(express.static('public'))
+    app.use('/uploads', express.static('public/img'));
 
-app.get('/', function (req, res) {
-    res.render('pages/index', {
-        imagesDB
-    }); //passiamo il databse immagini
-});
+    app.get('/', function (req, res) {
+        res.render('pages/index', {
+            imagesParsed
+        }); //passiamo il databse immagini
+    });
 
-app.post('/upload_photos', function (req, res) {
-    var photos = [],
-        form = new formidable.IncomingForm();
+    app.post('/upload_photos', function (req, res) {
+        var photos = [],
+            form = new formidable.IncomingForm();
 
-    // Tells formidable that there will be multiple files sent.
-    form.multiples = true;
-    // Upload directory for the images
-    form.uploadDir = path.join(__dirname, 'tmp_uploads');
+        // Tells formidable that there will be multiple files sent.
+        form.multiples = true;
+        // Upload directory for the images
+        form.uploadDir = path.join(__dirname, 'tmp_uploads');
 
-    // Invoked when a file has finished uploading.
-    form.on('file', function (name, file) {
+        // Invoked when a file has finished uploading.
+        form.on('file', function (name, file) {
 
 
-        var buffer = null,
-            type = null,
-            filename = '';
+            var buffer = null,
+                type = null,
+                filename = '';
 
-        // Read a chunk of the file.
-        buffer = readChunk.sync(file.path, 0, 260);
-        // Get the file type using the buffer read using read-chunk
+            // Read a chunk of the file.
+            buffer = readChunk.sync(file.path, 0, 260);
+            // Get the file type using the buffer read using read-chunk
 
-        FileType.fromBuffer(buffer).then(function (type) {
-            // Check the file type, must be either png,jpg or jpeg
-            if (type !== null && (type.ext === 'png' || type.ext === 'jpg' || type.ext === 'jpeg')) {
-                // Assign new file name
-                filename = Date.now() + '-' + file.name;
-                // Move the file with the new file name
-                fs.rename(file.path, path.join(__dirname, 'public/img/' + filename), function () { });
+            FileType.fromBuffer(buffer).then(function (type) {
+                // Check the file type, must be either png,jpg or jpeg
+                if (type !== null && (type.ext === 'png' || type.ext === 'jpg' || type.ext === 'jpeg')) {
+                    // Assign new file name
+                    filename = Date.now() + '-' + file.name;
+                    // Move the file with the new file name
+                    fs.rename(file.path, path.join(__dirname, 'public/img/' + filename), function () { });
 
-                // Add to the list of photos
-                photos.push({
-                    status: true,
-                    filename: filename,
-                    type: type.ext,
-                    publicPath: 'uploads/' + filename
-                });
-                console.log("upload");
-            } else {
-                photos.push({
-                    status: false,
-                    filename: file.name,
-                    message: 'Invalid file type'
-                });
-                fs.unlink(file.path);
-            }
+                    // Add to the list of photos
+                    photos.push({
+                        status: true,
+                        filename: filename,
+                        type: type.ext,
+                        publicPath: 'uploads/' + filename
+                    });
+                    console.log("upload");
+                } else {
+                    photos.push({
+                        status: false,
+                        filename: file.name,
+                        message: 'Invalid file type'
+                    });
+                    fs.unlink(file.path);
+                }
+            });
         });
 
+        form.on('error', function (err) {
+            console.log('Error occurred during processing - ' + err);
+        });
 
+        // Invoked when all the fields have been processed.
+        form.on('end', function () {
+            console.log('All the request fields have been processed.');
+        });
+
+        // Parse the incoming form fields.
+        form.parse(req, function (err, fields, files) {
+            res.status(200).json(photos);
+        });
     });
 
-    form.on('error', function (err) {
-        console.log('Error occurred during processing - ' + err);
+    app.listen(3000, function () {
+        console.log('Pixels web app listening on port 3000!');
     });
+}
 
-    // Invoked when all the fields have been processed.
-    form.on('end', function () {
-        console.log('All the request fields have been processed.');
-    });
-
-    // Parse the incoming form fields.
-    form.parse(req, function (err, fields, files) {
-        res.status(200).json(photos);
-    });
-});
-
-app.listen(3000, function () {
-    console.log('Pixels web app listening on port 3000!');
-});
+populateDB(main);
